8737a2a43660c0b0189e1152c390bc48fb7448f0
Bhaskar <annagiri.ps@gmail.com>
2019-08-30 23:56:54 +0530

    changes in master

--- src/com/sample/HelloClass.java
+++ src/com/sample/HelloClass.java
@@ -4,8 +4,9 @@ public class HelloClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("Sample file");
+		System.out.println("Sample file doing something else");
 		System.out.println("another statement");
+		System.out.println("third sttement");
 	}
 
 }
