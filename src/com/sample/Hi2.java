e2cde657b1b1513f8d7fb3720b1f5bb0a6eb02d3
Bhaskar <annagiri.ps@gmail.com>
2019-08-31 15:52:45 +0530

    s_01 commiting

--- src/com/sample/Hi2.java
+++ src/com/sample/Hi2.java
@@ -5,6 +5,7 @@ public class Hi2 {
 	public static void main(String[] args) {
 
 		System.out.println("saying Hii");
+		System.out.println("changing here");
 	}
 
 }
---
+++ src/com/sample/S_01.java
@@ -0,0 +1,9 @@
+package com.sample;
+
+public class S_01 {
+
+	public static void main(String[] args) {
+		System.out.println("another");
+	}
+
+}
