31c6e6d0f8af921326ab8b4752a14680d4b83f81
annagiri <45383958+annagiri@users.noreply.github.com>
2019-08-31 15:58:35 +0530

    update O

--- src/com/sample/S_01.java
+++ src/com/sample/S_01.java
@@ -3,7 +3,8 @@ package com.sample;
 public class S_01 {
 
 	public static void main(String[] args) {
-		System.out.println("another");
+		System.out.println("another time from O");
+		int i = 10;
 	}
 
 }
